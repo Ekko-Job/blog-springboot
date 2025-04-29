8577115702e4fe194746d2eba12bb9eced520a48
Ekko <ekko.zhang@unionftech.com>
2025-04-29 18:54:48 +0800

    [BLOG-338] | JSR 380 :BeanValidation

--- blog-web/src/main/java/com/ekko/controller/TestController.java
+++ blog-web/src/main/java/com/ekko/controller/TestController.java
@@ -3,9 +3,15 @@ package com.ekko.controller;
 import com.ekko.aspect.ApiOperationLog;
 import com.ekko.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 /**
  * TestController
@@ -19,9 +25,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @PostMapping("/test")
-    @ApiOperationLog(description = "Test Aspect Log")
-    public User test(@RequestBody User user) {
-        return user;
+    @ApiOperationLog(description = "测试接口")
+    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+
+        // 返参
+        return ResponseEntity.ok("参数没有任何问题");
     }
 
 }
--- blog-web/src/main/java/com/ekko/model/User.java
+++ blog-web/src/main/java/com/ekko/model/User.java
@@ -1,6 +1,13 @@
 package com.ekko.model;
 
+
 import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * User
@@ -13,7 +20,22 @@ import lombok.Data;
 public class User {
 
     // 用户名
+    @NotBlank(message = "用户名不能为空")
     private String username;
+
     // 性别
-    private String sex;
+    @NotNull(message = "性别不能为空")
+    private Integer sex;
+
+    // 年龄
+    @NotNull(message = "年龄不能为空")
+    @Min(value = 18, message = "年龄必须大于或等于 18")
+    @Max(value = 100, message = "年龄必须小于或等于 100")
+    private Integer age;
+
+    // 邮箱
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
+    private String email;
+
 }
