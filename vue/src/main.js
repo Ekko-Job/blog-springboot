409acc0954ad304160ee08761dfc393cba9eee13
Ekko <zhangshengkai2020cc@gmail.com>
2025-05-12 00:22:17 +0800

    [BLOG-443] | 整合 vue-router 路由管理器

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+// import './assets/main.css' // 引入 main.css 样式文件
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue' // 引入 createApp 方法
+import App from './App.vue' // 引入 App.vue 组件
 
-createApp(App).mount('#app')
+// 导入路由
+import router from './router'
+
+const app = createApp(App)
+
+// 应用路由
+app.use(router)
+app.mount('#app')
