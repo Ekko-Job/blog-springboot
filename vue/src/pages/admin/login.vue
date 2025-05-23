40969fd5b7d24c8442e0e37404d25e842ffd386e
Ekko <ekko.zhang@unionftech.com>
2025-05-23 18:01:16 +0800

    [BLOG-5515] | 重复登录问题优化、密码框可显示密码

--- vue/src/pages/admin/login.vue
+++ vue/src/pages/admin/login.vue
@@ -42,8 +42,7 @@ export default {
           </el-form-item>
           <el-form-item prop="password">
             <!-- 密码框组件 -->
-            <el-input size="large" type="password" v-model="form.password" placeholder="请输入密码"
-                      :prefix-icon="Lock" clearable/>
+            <el-input size="large" type="password" v-model="form.password" placeholder="请输入密码" :prefix-icon="Lock" clearable show-password/>
           </el-form-item>
           <el-form-item>
             <!-- 登录按钮，宽度设置为 100% -->
